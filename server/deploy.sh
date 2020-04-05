dab55d964da836e1ad1661cf72810e96d32b97e0
Travis Bumgarner <travis.bumgarner@gmail.com>
2020-04-05 14:13:25 -0400

    Updated deply script to set env varibales

--- server/deploy.sh
+++ server/deploy.sh
@@ -1,1 +1,7 @@
-gcloud builds submit --tag gcr.io/online-outings-273216/server && gcloud run deploy server --image gcr.io/online-outings-273216/server --platform managed --region us-east1;
+gcloud builds submit \
+    --tag gcr.io/online-outings-273216/server \
+&& gcloud run deploy server \
+    --image gcr.io/online-outings-273216/server \
+    --platform managed \
+    --region us-east1 \
+    --set-env-vars="IS_CLOUD_RUN=true"
